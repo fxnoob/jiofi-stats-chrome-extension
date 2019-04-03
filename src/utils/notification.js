e592d1523784b31562ab653fca6b97c73b8fd3fe
fxnoob <fxnoob71@gmail.com>
2019-04-04 02:28:35 +0530

    adding notification

--- src/utils/notification.js
+++ src/utils/notification.js
@@ -3,7 +3,6 @@ import { Sound } from './sound'
 const sound = new Sound()
 
 export class Notification {
-
   constructor (){
     this.notifiedOneTime = false;
   }
@@ -11,10 +10,23 @@ export class Notification {
     if ( json.battery_level < 11) {
       if(this.notifiedOneTime === false) {
         sound.play()
+        notify()
         this.notifiedOneTime = true;
       }
     } else {
       this.notifiedOneTime = false;
     }
   }
+}
+
+/**
+ * chrome notifications
+ * https://developer.chrome.com/extensions/desktop_notifications
+ * */
+const notify = () => {
+  const noti = webkitNotifications.createNotification(
+    'images/low_battery.png',
+    'Please connect Jiofi to Power!',  // notification title
+    'remaining power is less than 10 %'  // notification body text
+  );
 }
