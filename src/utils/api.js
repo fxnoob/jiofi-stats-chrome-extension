f39348871e7e09af2d014609c08780dd4a192c7b
fxnoob <fxnoob71@gmail.com>
2019-04-06 21:13:23 +0530

    multiple model support

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,6 @@
+import Db from './db'
+
+const db = new Db()
 
 export class Util {
   constructor () {
@@ -19,8 +22,15 @@ export class Util {
   }
 }
 
+const util = new Util()
+
 const getJioFiDeviceDetails = () => {
-  return fetch('http://jiofi.local.html/cgi-bin/qcmap_web_cgi', { 'credentials': 'omit', 'headers': { 'accept': 'text/plain, */*; q=0.01', 'accept-language': 'en-US,en;q=0.9,hi;q=0.8,te;q=0.7', 'cache-control': 'no-cache', 'content-type': 'application/x-www-form-urlencoded; charset=UTF-8', 'gfhfg': 'ghjhgjhgj', 'pragma': 'no-cache', 'x-requested-with': 'XMLHttpRequest', 'ytyty': 'fdghf' }, 'referrer': 'http://jiofi.local.html/', 'referrerPolicy': 'no-referrer-when-downgrade', 'body': 'Page=GetDeviceDetails&mask=0&token=0', 'method': 'POST', 'mode': 'cors' })
+  return db.get('modelNo')
+    .then((res) => {
+      const url = util.url[res.modelNo - 1].url
+      const configParam = util.url[res.modelNo - 1].data
+      return fetch(url, configParam)
+    })
     .then(res => {
       if (res.status === 200) {
         return res
